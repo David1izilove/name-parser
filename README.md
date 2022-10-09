8184e7444137930dba5ca866c210eb0d5c2c85d2
david <davidizilove@gmail.com>
2022-10-09 17:03:21 +0300

    First Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+*How To Run The Script*
+1. Install the [Python](https://www.python.org/downloads/)
+2. Go to a repository where you want to save the project - `cd <folder name>`
+3. Clone the repository - `git clone https://github.com/David1izilove/create_fake_users.git`
+4. Install virtual environment - `pip3 install virtualenv`
+5. Run virtual environment in the project directory- `virtualenv .`
+6. Activate virtual environment - `source venv/bin/activate`
+7. Install project dependencies - `pip3 install -r requirements.txt`
+8. Run the script - `python3 name_parser.py`
+9. The program will ask you to enter the name/s, you can enter in this format:
+    * Andrew
+    * David, Andrew, Thomas
---
+++ name_parser.py
@@ -0,0 +1,48 @@
+import re
+import os
+
+
+class Finder:
+
+    def __init__(self, text_file, names_list):
+        self.text_file = text_file
+        self.names_list = names_list
+
+    def read_txt_file(self):
+        """open and read txt file"""
+        if not os.stat(self.text_file).st_size == 0:
+            txt_file = open(self.text_file)
+            return txt_file
+        else:
+            raise Exception('The text file is empty')
+
+    def parser(self):
+        """this method will find the names in the text file"""
+        list_of_results = []
+        for name in self.names_list:
+            list_of_results.append((name + ' -> ' + '\n'))
+            for line_number, line_text in enumerate(self.read_txt_file()):
+                if name in line_text:
+                    line = str(line_number + 1)
+                    matches = re.finditer(name, line_text)
+                    matches_indexes = [match.start() for match in matches]
+                    result = "[lineNumber=" + line + ", " + "charIndex=" + \
+                             ','.join(str(index) for index in matches_indexes) + "] "
+                    list_of_results.append((result + '\n'))
+        return ''.join(list_of_results)
+
+    def result(self):
+        if self.parser():
+            return self.parser()
+        else:
+            return 'No results'
+
+
+file_path = 'text.txt'
+user_input = input('Enter name/s: ')
+users_list = user_input.replace(' ', '').split(',')
+
+finder = Finder(text_file=file_path, names_list=users_list)
+print(finder.result())
+
+
